29307fdde8531b3af0f6091c6861b359f09be32c
Anton Naumtsev <naumtsevanton@mail.ru>
2022-06-07 02:41:16 +0300

    update

--- build/generated/source/proto/main/grpc/ru/hse/LoginServiceGrpc.java
+++ build/generated/source/proto/main/grpc/ru/hse/LoginServiceGrpc.java
@@ -6,7 +6,7 @@ import static io.grpc.MethodDescriptor.generateFullMethodName;
  */
 @javax.annotation.Generated(
     value = "by gRPC proto compiler (version 1.47.0)",
-    comments = "Source: services.proto")
+    comments = "Source: login.proto")
 @io.grpc.stub.annotations.GrpcGenerated
 public final class LoginServiceGrpc {
 
